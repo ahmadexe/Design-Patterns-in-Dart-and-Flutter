8022c63490a04755dea9aa0d19b7d44c80b0344b
ahmadexe <muahmad710@gmail.com>
2023-02-14 09:50:51 +0500

    dart code for factory pattern added

---
+++ factory/dart/main.dart
@@ -0,0 +1,54 @@
+/*
+  Remember folks
+  * Inherit from abstraction.
+  * Create enums, don't rely on string.
+  * Follow Liskov's substitution principal: Child classes can be freely used with parent as type.
+*/
+
+enum LaptopBrand {
+  apple,
+  dell,
+  hp,
+}
+
+abstract class Laptop {
+  factory Laptop(LaptopBrand brand) {
+    switch (brand) {
+      case LaptopBrand.apple:
+        return Apple();
+      case LaptopBrand.dell:
+        return Dell();
+      case LaptopBrand.hp:
+        return Hp();
+    }
+  }
+
+  void os();
+}
+
+class Apple implements Laptop {
+  @override
+  void os() {
+    print("MacOS");
+  }
+
+}
+
+class Dell implements Laptop {
+  @override
+  void os() {
+    print('Linux');
+  }
+}
+
+class Hp implements Laptop {
+  @override
+  void os() {
+    print('Windows');
+  }
+}
+
+void main(List<String> args) {
+  Laptop laptop = Laptop(LaptopBrand.apple);
+  laptop.os();
+}
