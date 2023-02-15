d3b39b4350714a53a20ef18915e0dc80ae25e1f7
ahmadexe <muahmad710@gmail.com>
2023-02-15 10:20:49 +0500

    platform indicator added

--- abstract_factory/flutter_abstract_factory/lib/home_screen.dart
+++ abstract_factory/flutter_abstract_factory/lib/home_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_abstract_factory/platform_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            PlatformButton(Theme.of(context).platform).build(
+              () {
+                debugPrint("Hello");
+              },
+              const Text('Hello'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ abstract_factory/flutter_abstract_factory/lib/platform_progress_indicator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class PlatformIndicator {
+  Widget build();
+  factory PlatformIndicator(TargetPlatform platform) {
+    switch (platform) {
+      case TargetPlatform.android:
+        return AndroidIndicator();
+      case TargetPlatform.iOS:  
+        return IOSIndicator();
+      default:
+        return IOSIndicator();
+    }
+  }
+}
+
+class AndroidIndicator implements PlatformIndicator {
+  @override
+  Widget build() {
+    return const CircularProgressIndicator();
+  }
+}
+
+class IOSIndicator implements PlatformIndicator {
+  @override
+  Widget build() {
+    return const CupertinoActivityIndicator();
+  }
+}
